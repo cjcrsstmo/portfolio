52b51e5c72e770ae0e4396f1dec35051d07168f7
CJ Crisostomo <25crisostomocj@gmail.com>
2025-04-03 23:04:45 +0800

    fixed project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,42 +1,68 @@
-// import CJ from '../assets/cj.jpg'
+import CJ from '../assets/cj.jpg'
+import objectiva from '../assets/objectiva.jpg'
+import dentalcare from '../assets/dentalcare.jpg'
+import bookstore from '../assets/bookstore.jpg'
+import {useState} from 'react'
+
+
 const Projects = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const toggleProject = () => {
+        setIsOpen(!isOpen)
+    }
+
   return (
     <section className='min-h-screen pt-10 xl:pt-20' id='projects'>
                 <h1 className='text-3xl sm:text-4xl md:text-5xl text-center mb-4' data-aos="zoom-in-up">PROJECTS</h1>
                 <p className='text-center' data-aos="zoom-in-up">Explore the projects I've worked on so far</p>
 
-                <p className='text-center text-2xl mt-50' data-aos="zoom-in-up">Temporarily Unavailable.</p>
-
-                {/* <div className='flex flex-col sm:flex-row justify-center items-center mx-auto w-2/3 gap-10 sm:gap-20 md:gap-30 lg:gap-40 mt-10 xl:mt-20'>
-                        <img src={CJ} alt="" className='w-2/7 rounded-xl' data-aos="fade-right"/>
-
-                    <div className='w-2/5 h-50' data-aos="fade-left">
-                        <hr /> <br />
-                        <hr />
-                        <div className='flex items-center justify-center sm:justify-around p-4'>
-                            <p className='hidden sm:block'><i className='bx bx-arrow-back text-lg sm:text-2xl' /></p>
-                            <p className='text-xl sm:text-2xl'>Obejctiva</p>
-                        </div>
-                        <hr />
-
-                        <div className='flex items-center justify-center sm:justify-around p-4'>
-                            <p className='hidden sm:block'><i className='bx bx-arrow-back text-lg sm:text-2xl' /></p>
-                            <p className='text-xl sm:text-2xl'>Obejctiva</p>
-                        </div>
-                        <hr />
-
-                        <div className='flex items-center justify-center sm:justify-around p-4'>
-                            <p className='hidden sm:block'><i className='bx bx-arrow-back text-lg sm:text-2xl' /></p>
-                            <p className='text-xl sm:text-2xl'>Obejctiva</p>
-                        </div>
-                        <hr />
-
-                        <div className='flex items-center justify-center sm:justify-around p-4'>
-                            <p className='hidden sm:block'><i className='bx bx-arrow-back text-lg sm:text-2xl' /></p>
-                            <p className='text-xl sm:text-2xl'>Obejctiva</p>
+                <div className='flex flex-col sm:flex-row justify-center items-center mx-auto w-2/3 gap-15 mt-10 xl:mt-20'>
+                    <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
+                        <figure>
+                            <img
+                            src={objectiva} alt="objectiva" className='rounded-lg'/>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mx-auto">Objectiva</h2>
+                            <p><strong>Description:</strong> Our capstone project which is a interactive learning platform for learning the 4 basic principles of OOP in Java</p><p className='mt-2'><strong>Tech Stack:</strong> Angular, CSS, Bootstrap, Laravel, MySql</p>
+                            <div className="card-actions justify-end">
+                            <a href='https://objectiva.tech' target='_blank' className="btn btn-outline btn-accent mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
+                            </div>
                         </div>
                     </div>
-                </div> */}
+
+                    <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
+                        <figure>
+                            <img
+                            src={dentalcare} alt="objectiva" className='rounded-lg'/>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mx-auto">Dental Care</h2>
+                            <p><strong>Description:</strong> A dental care website that is offering an easy-to-use interface for booking appointments and submitting reviews to enhance overall patient experience and care.</p><p className='mt-2'><strong>Tech Stack:</strong> HTML, Bootstrap, Javascript</p>
+                            <div className="card-actions justify-end">
+                            <a href='https://cjcrsstmo.github.io/DentalCare/' target='_blank' className="btn btn-outline btn-accent mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
+                        <figure>
+                            <img src={bookstore} alt="objectiva" className=''/>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mx-auto">Bookstore201</h2>
+                            <p><strong>Description:</strong> A library website for browsing books</p><p className='mt-2'><strong>Tech Stack:</strong> Angular, CSS, Bootstrap, Laravel, MySql</p>
+                            <div className="card-actions justify-end">
+                            <a href='https://cjcrsstmo.github.io/Bookstore201/' target='_blank' className="btn btn-outline btn-accent mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+               
+
     </section>
   )
 }
