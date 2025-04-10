8c64fae24eaf8cbe4d588bfb3d18f2450ec038d9
unknown <25crisostomocj@gmail.com>
2025-04-10 22:05:11 +0800

    updated projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -23,9 +23,9 @@ const Projects = () => {
                 <p className='text-center' data-aos="zoom-in-up">Explore the projects I've worked on so far</p>
 
                 <div className='flex flex-col lg:flex-row justify-center items-center mx-auto w-full md:w-2/3 gap-8 sm:gap-10 md:gap:15 mt-10 xl:mt-20'>
-
-                <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100' >
-                <p className="mx-auto mb-3 text-xl font-bold">Objectiva</p>
+                    {/* Objectiva */}
+                    <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100' data-aos="zoom-in">
+                        <p className="mx-auto mb-3 text-xl font-bold">Objectiva</p>
                         <p className='text-justify'><strong>Description:</strong> Our capstone project which is an interactive learning platform for learning the 4 basic principles of OOP in Java</p>
                         <p className='mt-2 text-start w-full flex gap-2 items-center justify-start'>
                             <strong>Tech Stack:</strong> <i className='bx bxl-angular text-2xl text-primary '/> <i className='bx bxl-css3 text-2xl text-primary '/> <i className='bx bxl-bootstrap text-2xl text-primary '/> <i className='fab fa-laravel text-xl text-primary '/> <i className='fa-solid fa-database text-xl text-primary '/>
@@ -34,8 +34,8 @@ const Projects = () => {
                             <a href='https://objectiva.tech' target='_blank' className="btn btn-outline btn-primary mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
                         </div>
                     </div>
-
-                    <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100 ' >
+                    {/* Dental Care */}
+                    <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100' data-aos="zoom-in">
                         <p className="mx-auto mb-3 text-xl font-bold">Dental Care</p>
                         <p className='text-justify'><strong>Description:</strong> A dental care website that is offering an easy-to-use interface for booking appointments and submitting reviews.</p>
                         <p className='mt-2 text-start w-full flex gap-2 items-center justify-start'>
@@ -45,8 +45,8 @@ const Projects = () => {
                             <a href='https://cjcrsstmo.github.io/DentalCare/' target='_blank' className="btn btn-outline btn-primary  mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
                         </div>
                     </div>
-
-                    <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100 ' >
+                    {/* Bookstore201 */}
+                    <div className='flex flex-col justify-around items-center bg-primary-content rounded-xl p-10 w-8/10 sm:w-5/10 md:w-3/4 lg:w-2/5 min-h-100 max-h-100' data-aos="zoom-in">
                         <p className="mx-auto mb-3 text-xl font-bold">Bookstore201</p>
                         <p className='text-justify'><strong>Description:</strong> A library website for browsing books.</p>
                         <p className='mt-2 text-start w-full flex gap-2 items-center justify-start'>
@@ -56,46 +56,6 @@ const Projects = () => {
                             <a href='https://cjcrsstmo.github.io/Bookstore201/' target='_blank' className="btn btn-outline btn-primary  mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
                         </div>
                     </div>
-                    {/* <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
-                        <figure>
-                            <img
-                            src={objectiva} alt="objectiva" className='rounded-lg'/>
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title mx-auto">Objectiva</h2>
-                            <p><strong>Description:</strong> Our capstone project which is an interactive learning platform for learning the 4 basic principles of OOP in Java</p><p className='mt-2'><strong>Tech Stack:</strong> Angular, CSS, Bootstrap, Laravel, MySql</p>
-                            <div className="card-actions justify-end">
-                            <a href='https://objectiva.tech' target='_blank' className="btn btn-outline btn-primary mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
-                        <figure>
-                            <img
-                            src={dentalcare} alt="objectiva" className='rounded-lg'/>
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title mx-auto">Dental Care</h2>
-                            <p><strong>Description:</strong> A dental care website that is offering an easy-to-use interface for booking appointments and submitting reviews to enhance overall patient experience and care.</p><p className='mt-2'><strong>Tech Stack:</strong> HTML, Bootstrap, Javascript</p>
-                            <div className="card-actions justify-end">
-                            <a href='https://cjcrsstmo.github.io/DentalCare/' target='_blank' className="btn btn-outline btn-primary mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card bg-base-100 image-full w-96 shadow-sm" data-aos="zoom-in-up">
-                        <figure>
-                            <img src={bookstore} alt="objectiva" className=''/>
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title mx-auto">Bookstore201</h2>
-                            <p><strong>Description:</strong> A library website for browsing books</p><p className='mt-2'><strong>Tech Stack:</strong> Angular, CSS, Bootstrap, Laravel, MySql</p>
-                            <div className="card-actions justify-end">
-                            <a href='https://cjcrsstmo.github.io/Bookstore201/' target='_blank' className="btn btn-outline btn-primary mx-auto mt-5"><i className='bx bx-link' ></i>Visit Site</a>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
 
                
