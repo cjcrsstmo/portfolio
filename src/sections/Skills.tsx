9eddf07300e9c1a47d4546f3265365e024409a4a
unknown <25crisostomocj@gmail.com>
2025-04-10 17:01:54 +0800

    fixed skill's icon

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -20,12 +20,12 @@ const Skills = () => {
                       {/* <h1 className='text-2xl'>Front-End</h1> */}
                           <ul className='flex gap-10 mt-4 justify-evenly w-4/5 mx-auto text-center flex-wrap'>
                               <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-css3 text-7xl text-accent mb-2 opacity-80'/>CSS</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-javascript text-7xl text-accent mb-2 opacity-80'/>JAVASCRIPT</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-typescript text-7xl text-accent mb-2 opacity-80'/>TYPESCRIPT</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-bootstrap text-7xl text-accent mb-2 opacity-80'/>BOOTSTRAP</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-tailwind-css text-7xl text-accent mb-2 opacity-80'/>TAILWIND</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-angular text-7xl text-accent mb-2 opacity-80'/>ANGULAR</li>
-                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-react text-7xl text-accent mb-2 opacity-80'/>REACT</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-javascript text-7xl text-accent mb-2 opacity-80'/>Javascript</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-typescript text-7xl text-accent mb-2 opacity-80'/>Typescript</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-bootstrap text-7xl text-accent mb-2 opacity-80'/>Bootstrap</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-tailwind-css text-7xl text-accent mb-2 opacity-80'/>TailwindCSS</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-angular text-7xl text-accent mb-2 opacity-80'/>Angular</li>
+                              <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-react text-7xl text-accent mb-2 opacity-80'/>React</li>
                           </ul>
                   </div>
   <hr className='w-1/2 mx-auto mt-10' data-aos="fade-up"/>
@@ -34,10 +34,10 @@ const Skills = () => {
                   <div className='mt-10' data-aos="fade-up">
                   {/* <h1 className='text-2xl'>Back-End</h1> */}
                       <ul className='flex gap-10 mt-4 justify-evenly w-4/5 mx-auto text-center flex-wrap'>
-                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-php text-7xl text-accent mb-2 opacity-80'/>PHP</li>
-                          <li className='flex flex-col text-accent opacity-80'><i className='fab fa-laravel text-7xl text-accent mb-2 opacity-80'/>LARAVEL</li>
-                          <li className='flex flex-col text-accent opacity-80'><i className='fa-solid fa-database text-7xl text-accent mb-2 opacity-80'/>MYSQL</li>
-                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-firebase text-7xl text-accent mb-2 opacity-80'/>FIREBASE</li>
+                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-php text-7xl text-accent mb-2 opacity-80'/>Php</li>
+                          <li className='flex flex-col text-accent opacity-80'><i className='fab fa-laravel text-7xl text-accent mb-2 opacity-80'/>Laravel</li>
+                          <li className='flex flex-col text-accent opacity-80'><i className='fa-solid fa-database text-7xl text-accent mb-2 opacity-80'/>MySql</li>
+                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-firebase text-7xl text-accent mb-2 opacity-80'/>Firebase</li>
                       </ul>
                   </div>
   <hr className='w-1/2 mx-auto mt-10' data-aos="fade-up"/>
@@ -45,9 +45,9 @@ const Skills = () => {
                   <div className='mt-10' data-aos="fade-up">
                   {/* <h1 className='text-2xl'>Others</h1> */}
                       <ul className='flex gap-10 mt-4 justify-evenly w-4/5 mx-auto text-center flex-wrap'>
-                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-git text-7xl text-accent mb-2 opacity-80'/>GIT</li>
-                          <li className='flex flex-col text-accent opacity-80'><img src="https://img.icons8.com/?size=80&id=KIcFwp9MNQL5&format=png&color=00D3BB" alt="" className="pb-1" />POSTMAN</li> 
-                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-figma text-7xl text-accent mb-2 opacity-80'/>FIGMA</li>
+                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-git text-7xl text-accent mb-2 opacity-80'/>Git</li>
+                          <li className='flex flex-col text-accent opacity-80'><img src="https://img.icons8.com/?size=80&id=KIcFwp9MNQL5&format=png&color=00D3BB" alt="" className="pb-1" />Postman</li> 
+                          <li className='flex flex-col text-accent opacity-80'><i className='bx bxl-figma text-7xl text-accent mb-2 opacity-80'/>Figma</li>
                       </ul>
                   </div>
               </div>
