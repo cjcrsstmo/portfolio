79d07e911c6d45965a630410da5ea35fe63f26cc
unknown <25crisostomocj@gmail.com>
2025-04-12 15:58:40 +0800

    new link integrated

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';  
 import { useEffect } from 'react';
 import 'boxicons/css/boxicons.min.css';
-import Background from './assets/bg-portfolio.jpg'
+import Background from './assets/opoy7.jpg'
 
 function App() {
   
@@ -23,14 +23,16 @@ function App() {
 
   return (
     <>
-      <div className='bg-cover bg-center bg-fixed	min-h-screen bg-no-repeat' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${Background})` }}>
+      <div className='bg-cover bg-center bg-fixed	min-h-screen bg-no-repeat' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${Background})` }}>
           <Navbar/>
-          <Landing/>
+
+          <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>New Link: <a href="https://cjcrsstmo.vercel.app" target='_blank' className='text-underline'>cjcrsstmo.vercel.app</a></p>
+          {/* <Landing/>
           <AboutMe/>
           <Skills/>
           <Projects/>
           <Contact />
-          <Footer />
+          <Footer /> */}
       </div>
     </>
   )
